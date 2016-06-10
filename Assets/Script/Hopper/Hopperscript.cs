acb4681533245387bb3879f503c1cc7cd9bbaf97
Dh0le <zhu442@purdue.edu>
2016-06-10 20:40:28 +0800

    Add the teleport base at the bottom of the track.

---
+++ Assets/Script/Hopper/Hopperscript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hopperscript : MonoBehaviour {
+
+    void start()
+    {
+        Rigidbody rb = new Rigidbody();
+    }
+    void update(){
+           
+    }
+}
