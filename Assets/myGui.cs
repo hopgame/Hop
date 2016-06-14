80a15b3c8600cf660589df3afeb607f85523882b
Bowie Gou <Bowie Gou>
2016-06-14 20:11:29 +0800

    init menu scene

---
+++ Assets/myGui.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myGui : MonoBehaviour {
+
+    void onGui() {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button"))
+            print("You clicked the button!");
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
