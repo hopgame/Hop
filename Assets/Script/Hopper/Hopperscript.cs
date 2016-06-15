839633f136c40111e003fa8f5ef09d862573e1ae
Dh0le <zhu442@purdue.edu>
2016-06-16 00:36:50 +0800

    add the script of collision to teleport pad(not yet finished,when the collision happen,errors will happen)

--- Assets/Script/Hopper/Hopperscript.cs
+++ Assets/Script/Hopper/Hopperscript.cs
@@ -2,12 +2,32 @@
 using System.Collections;
 
 public class Hopperscript : MonoBehaviour {
-
+    public Rigidbody rb;
+    private bool up = false;
     void start()
     {
-        Rigidbody rb = new Rigidbody();
+        rb = this.GetComponent<Rigidbody>();
+       
     }
     void update(){
+
            
+    }
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log("Hiting something");
+        if (collisionInfo.gameObject.tag.CompareTo("Beatspad") == 0)
+        {
+            Debug.Log("Hit the pad");
+            rb.useGravity = false;
+            Vector3 vertical = new Vector3(0.0f, 1.0f, 0.0f);
+            rb.AddForce(vertical);
+        }
+        if (collisionInfo.gameObject.tag.CompareTo("Top") == 0)
+        {
+            rb.useGravity = true;
+
+        }
+        
     }
 }
