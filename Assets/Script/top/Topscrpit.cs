35b69bba69527084bf12b45544009d051e2463ba
Dh0le <zhu442@purdue.edu>
2016-06-26 17:39:23 +0800

    hopper can now hop up and down between pad and top.

---
+++ Assets/Script/top/Topscrpit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Topscrpit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    private Rigidbody rb;
+    //private GameObject hp;
+    private ConstantForce cf;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        // Debug.Log("Hiting something");
+        if (collisionInfo.gameObject.tag.CompareTo("Hopper") == 0)
+        {
+            rb = collisionInfo.rigidbody;
+            rb.useGravity = true;
+            Vector3 vertical = new Vector3(0.0f, 0.0f, 0.0f);
+            //rb.velocity = vertical;
+            //hp = collisionInfo.gameObject;
+            cf = rb.GetComponent<ConstantForce>();
+            cf.force = vertical;
+
+            Debug.Log("Finish the process");
+        }
+    }
+}
