e5033cceae1f5cc1bbf3661559e8d367d1682a6f
bowiegou <i@bowiegou.com>
2016-06-28 01:21:35 +0800

    Refactor a ton, added basic logic for generation of track and game UI object

---
+++ Assets/Editor/CameraEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(Cameracontroller))]
+public class CameraEditor : Editor {
+    public override void OnInspectorGUI() {
+        Cameracontroller controller = (Cameracontroller) target;
+        if (DrawDefaultInspector()) {
+            controller.DebugScene();
+        }
+        if (GUILayout.Button("Generate")) {
+            controller.DebugScene();
+        }
+    }
+}
